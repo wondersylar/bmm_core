9a27025774494895da063da33bf08a7ff25c5c9e
wondersylar <66655309+wondersylar@users.noreply.github.com>
2021-11-02 09:49:37 +0800

    Delete scripts directory
    to be done

--- scripts/elf2verilog.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-import sys,os,re
-
-for root, dirs, files in os.walk("/home/songxue/pcl_mcu_20210423/pcl_mcu/tests/isa_csr_00/elf/", topdown=False):
-    for name in files:
-        print('filename --->',name )
-        os.system("riscv64-unknown-elf-objcopy -O verilog " +os.path.join(root, name) + " /home/songxue/pcl_mcu_20210423/pcl_mcu/tests/isa_csr_00_init/" + name + ".verilog")
-        print('transform OK --->',name )
